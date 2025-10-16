9754f64ae05f3d9f959ec01a743ae13811b94065
Che Zheng <Zhengche30@outlook.com>
2025-10-16 20:26:10 +0800

    1

---
+++ .github/prompts/system prompt.prompt.md
@@ -0,0 +1,1 @@
+1、系统为Windows，请使用powershell支持的命令。不得使用&&命令。
---
+++ sd/encoder.py
@@ -0,0 +1,43 @@
+from statistics import variance
+import torch
+from torch import nn
+from torch.nn import functional as F
+from decoder import VAE_AttentionBlock, VAE_ResidualBlock
+
+
+class VAE_Encoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__(
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+            nn.Conv2d(128, 128, 3, 2, padding=0),
+            VAE_ResidualBlock(128, 256),
+            VAE_ResidualBlock(256, 256),
+            nn.Conv2d(256, 256, 3, 2, padding=0),
+            VAE_ResidualBlock(256, 512),
+            VAE_ResidualBlock(512, 512),
+            nn.Conv2d(512, 512, 3, 2, padding=0),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512, 512),
+            nn.GroupNorm(32, 512),
+            nn.SiLU(),
+            nn.Conv2d(512, 8, kernel_size=3, padding=1),
+            nn.Conv2d(8, 8, kernel_size=1, padding=0),
+        )
+
+    def forward(self, x: torch.Tensor, noise: torch.Tensor) -> torch.Tensor:
+        for module in self:
+            if getattr(module, "stride", None) == (2, 2):
+                x = F.pad(x, (1, 0, 1, 0))
+            x = module(x)
+        mean, log_variance = torch.chunk(x, 2, dim=1)
+        log_variance = torch.clamp(log_variance, -30, 20)
+        variance = torch.exp(log_variance)
+        stdev = torch.sqrt(variance)
+        x = mean + stdev * noise
+        x *= 0.18215
+        return x
